acff6e84a532504dbb70567742407cda4cde6f29
James Thomas <mrcloudmustache@gmail.com>
2025-07-09 21:30:39 -0500

    Allow multiple instances and Name Tag

--- modules/ec2_instance/main.tf
+++ modules/ec2_instance/main.tf
@@ -1,6 +1,12 @@
 resource "aws_instance" "hello_world" {
-    ami = var.ami
-    subnet_id = var.subnet_id
-    instance_type = var.instance_type
-  
+  count = var.instance_count
+
+  ami           = var.ami
+  subnet_id     = var.subnet_id
+  instance_type = var.instance_type
+
+  tags = merge(var.tags, {
+    Name = "${var.name_prefix}-${count.index}"
+  })
+
 }
